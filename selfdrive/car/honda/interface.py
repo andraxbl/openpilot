c967633a4c489f81a628b2891da86f9eb4d8b2bb
andraxbl <andraxbl@gmail.com>
2025-05-29 05:52:04 -0500

    Fix indentation in interface.py for CR-V Hybrid Bosch

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -163,12 +163,12 @@ class CarInterface(CarInterfaceBase):
       ret.wheelSpeedFactor = 1.025
 
     elif candidate in (CAR.HONDA_CRV_HYBRID, CAR.HONDA_CRV_HYBRID_6G, CAR.HONDA_CRV_6G):
-  ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 5000], [0, 5000]]
-  ret.lateralTuning.pid.kpBP = [[0, 10]]
-  ret.lateralTuning.pid.kpV  = [[0.06, 0.58]]
-  ret.lateralTuning.pid.kiBP = [[0, 10]]
-  ret.lateralTuning.pid.kiV  = [[0.015, 0.14]]
-  ret.wheelSpeedFactor = 1.025
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 5000], [0, 5000]]
+      ret.lateralTuning.pid.kpBP = [[0, 10]]
+      ret.lateralTuning.pid.kpV  = [[0.06, 0.58]]
+      ret.lateralTuning.pid.kiBP = [[0, 10]]
+      ret.lateralTuning.pid.kiV  = [[0.015, 0.14]]
+      ret.wheelSpeedFactor = 1.025
 
     elif candidate == CAR.HONDA_FIT:
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
