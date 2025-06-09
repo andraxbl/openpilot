44345d7ccf45618a5ae74300d1b8d712c116f53f
andraxbl <andraxbl@gmail.com>
2025-06-08 22:45:38 -0500

    Update developer_panel.cc

--- selfdrive/ui/qt/offroad/developer_panel.cc
+++ selfdrive/ui/qt/offroad/developer_panel.cc
@@ -80,7 +80,7 @@ void DeveloperPanel::updateToggles(bool _offroad) {
      */
     experimentalLongitudinalToggle->setVisible(CP.getAlphaLongitudinalAvailable() && !is_release);
 
-    longManeuverToggle->setEnabled(true)
+    longManeuverToggle->setEnabled(true);
   } else {
     longManeuverToggle->setEnabled(true);
     experimentalLongitudinalToggle->setVisible(true);
